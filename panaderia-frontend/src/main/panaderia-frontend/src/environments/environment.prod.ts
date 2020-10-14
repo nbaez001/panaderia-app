edc401c37d638a1b13dd7e8ea299184e02702673
Nerio Baez <nbaez001@gmail.com>
2020-10-13 23:40:50 -0500

    [develop] - [modificaciones para despliegue en local]

--- panaderia-frontend/src/main/panaderia-frontend/src/environments/environment.prod.ts
+++ panaderia-frontend/src/main/panaderia-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  WsPanaderiaBackend: 'http://localhost:8081/panaderia-backend',
-  WsPanaderiaAuthorizer: 'http://localhost:8081/panaderia-authorizer',
+  WsPanaderiaBackend: 'http://localhost:8080/panaderia-backend',
+  WsPanaderiaAuthorizer: 'http://localhost:8080/panaderia-authorizer',
   WsReniec: 'http://perficon.elnazarenovraem.edu.pe/api/soap/persona',
   clientId: 'pseg',
   clientSecret: 'pseg@2020'
